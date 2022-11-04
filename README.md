6e438ff6759fa145d388bce638c66061677d5d4d
Seb Beattie <sebastian.beattie@bbc.co.uk>
2022-11-04 14:41:53 +0000

    Added some code

---
+++ README.md
@@ -0,0 +1,5 @@
+# Snazzifier
+
+A bash script to spice up the usual boring look of a bash shell
+
+Simply run `bash snazzify.sh`
---
+++ snazzify.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo curl -o /usr/bin/neofetch https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch
+sudo chmod +x /usr/bin/neofetch
+echo "PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
+echo "neofetch" >> ~/.bashrc
+source ~/.bashrc
