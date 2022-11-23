b8ca50e6d823f911b15c036823ca6b483880efc1
Seb Beattie <sebastian.beattie@bbc.co.uk>
2022-11-23 16:25:45 +0000

    Added better autocompletion

--- snazzify.sh
+++ snazzify.sh
@@ -3,4 +3,6 @@ sudo curl -o /usr/bin/neofetch https://raw.githubusercontent.com/dylanaraps/neof
 sudo chmod +x /usr/bin/neofetch
 echo "PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
 echo "neofetch" >> ~/.bashrc
+echo "bind 'set show-all-if-ambiguous on'" >> ~/.bashrc
+echo "bind 'TAB:menu-complete'" >> ~/.bashrc
 source ~/.bashrc
