5e872e1ae46311e9d41cbbf5dc0e34f110b3bae2
Seb Beattie <114009274+sebastianbeattie@users.noreply.github.com>
2023-01-05 12:11:45 +0000

    Update snazzify.sh

--- snazzify.sh
+++ snazzify.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo curl -o /usr/bin/neofetch https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch
 sudo chmod +x /usr/bin/neofetch
-echo "PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
-echo "neofetch" >> ~/.bashrc
-echo "bind 'set show-all-if-ambiguous on'" >> ~/.bashrc
-echo "bind 'TAB:menu-complete'" >> ~/.bashrc
-source ~/.bashrc
+echo "PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bash_profile
+echo "neofetch" >> ~/.bash_profile
+echo "bind 'set show-all-if-ambiguous on'" >> ~/.bash_profile
+echo "bind 'TAB:menu-complete'" >> ~/.bash_profile
+source ~/.bash_profile
